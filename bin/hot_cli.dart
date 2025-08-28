0cf316becfa7bbc6cd8a44e5804725b0ad24a1bf
aliraswli <aliraswli.business@gmail.com>
2025-08-28 16:39:30 +0330

    Add command

--- bin/hot_cli.dart
+++ bin/hot_cli.dart
@@ -2,11 +2,10 @@ import 'package:args/command_runner.dart';
 import 'package:hot_cli/commands/make_feature_command.dart';
 
 Future<void> main(List<String> args) async {
-  print("DDDDDDDDDDDDDDDDDD");
-  // final runner = CommandRunner<int>(
-  //   'hot_cli',
-  //   'Custom CLI with mason',
-  // )..addCommand(MakeFeatureCommand());
-  //
-  // await runner.run(args);
+  final runner = CommandRunner<int>(
+    'hot_cli',
+    'Custom CLI with mason',
+  )..addCommand(MakeFeatureCommand());
+
+  await runner.run(args);
 }
