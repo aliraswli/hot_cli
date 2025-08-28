faff376fc6c0ad6d7b1418d4e27b6a1b8c8cfa7c
aliraswli <aliraswli.business@gmail.com>
2025-08-28 14:58:14 +0330

    First Commit

---
+++ bin/hot_cli.dart
@@ -0,0 +1,5 @@
+import 'package:hot_cli/hot_cli.dart' as hot_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hot_cli.calculate()}!');
+}
---
+++ test/hot_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:hot_cli/hot_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
